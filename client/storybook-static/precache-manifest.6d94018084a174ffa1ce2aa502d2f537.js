8ac9ca83aff7c7e2a498c4ff7307bc62f87c96d7
tylerccaceres <tyler.caceres@gmail.com>
2019-08-31 16:14:09 -0400

    Add styles

---
+++ client/storybook-static/precache-manifest.6d94018084a174ffa1ce2aa502d2f537.js
@@ -0,0 +1,15 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d64b0d83a551f20db1d1a5addeb8c005",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.e0b37f7986c22aeab2ad.bundle.js"
+  },
+  {
+    "url": "runtime~main.e0b37f7986c22aeab2ad.bundle.js"
+  },
+  {
+    "url": "vendors~main.e0b37f7986c22aeab2ad.bundle.js"
+  }
+]);
