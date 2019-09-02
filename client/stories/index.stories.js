a0a0519023883c970268edd7c6b10472502e2615
Selin Huang <selin.hyx@gmail.com>
2019-09-02 14:07:45 -0400

    cleaned merge conflicts

--- client/stories/index.stories.js
+++ client/stories/index.stories.js
@@ -4,12 +4,14 @@ import ChatSettingsButton from "./sub-stories/chatSettingsButton";
 import ChatSearchBar from "./sub-stories/chatSearchBar";
 import ChatAvatar from "./sub-stories/chatAvatar";
 import ContactListItems from "./sub-stories/contactListItems";
+import ContactList from "./sub-stories/contactList";
 
 ContactButton();
 ChatSettingsButton();
 ChatSearchBar();
 ChatAvatar();
 ContactListItems();
+ContactList();
 // selin **********
 
 // tyler **********
