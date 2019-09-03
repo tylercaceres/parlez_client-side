e7fa7dd01c3e7e2aeba9785acce4063c84a7a052
tylerccaceres <tyler.caceres@gmail.com>
2019-09-03 02:31:42 -0400

    user concern, allow to retrieve current active user easily

--- serverv2/app/controllers/sessions_controller.rb
+++ serverv2/app/controllers/sessions_controller.rb
@@ -1,4 +1,7 @@
 class SessionsController < ApplicationController
+  include CurrentUserConcern
+  # has the logic for finding the current user
+
   def create
     user = User
       .find_by(email: params["user"]["email"])
@@ -14,4 +17,18 @@ class SessionsController < ApplicationController
       render json: {status: 401}
     end
   end
+
+  def logged_in
+    if @current_user
+      render json: {logged_in: true, user: @current_user}
+    else
+      render json: {logged_in: false}
+    end
+  end
+
+  def logout
+    reset_session
+    render json: {status: 200, logged_out: true}
+  end
+
 end
