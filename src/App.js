eb2399096122534af9a76c23e272ec1852f78fcd
Selin Huang <selin.hyx@gmail.com>
2019-09-05 07:58:30 -0400

    included homepage on app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from "react";
 import LoginPage from "../src/components/login/LoginPage";
 import HomePage from "../src/components/HomePage";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
-  return <HomePage />;
+  return (
+    <HomePage />
+    // <div>
+    //   <BrowserRouter>
+    //     <Switch>
+    //       <Route exact path={"/login"} render={<LoginPage />}></Route>
+    //       <Route exact path={"/"} render={<HomePage />}></Route>
+    //     </Switch>
+    //   </BrowserRouter>
+    // </div>
+  );
 }
 
 export default App;
