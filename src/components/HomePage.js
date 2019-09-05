d71e782887f7bf4f113a8e8db6d700c77f43902b
Selin Huang <selin.hyx@gmail.com>
2019-09-05 07:29:37 -0400

    modified home page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -11,19 +11,175 @@ const chatrooms = [
     id: 1,
     type: "group",
     name: "Anchen & Tyler",
-    avatar: "/avatar/one.png"
+    avatar: "/avatar/one.png",
+    messages: [
+      {
+        id: 0,
+        creator: "tyler",
+        content: "what whattt",
+        created_at: "20 sep 2019 5pm",
+        deleted: false
+      },
+      {
+        id: 1,
+        creator: "bob",
+        content: "hello there",
+        created_at: "20 sep 2019 2pm",
+        deleted: true
+      },
+      {
+        id: 2,
+        creator: "christopher",
+        content: "wow, i hate you.",
+        created_at: "20 sep 2019 3pm",
+        deleted: false
+      },
+      {
+        id: 3,
+        creator: "anchen",
+        content: "i only drink sweet drinks.",
+        created_at: "20 sep 2019 4pm",
+        deleted: true
+      },
+      {
+        id: 4,
+        creator: "tyler",
+        content:
+          "have some gatorade, you look thirsty blah daoisdjasodjiausd holy moly my computer is so slow. help i need to clean it up i think sty blah daoisdjasodjiausd holy moly my computer is so slow. help i need to clean it up i thinksty blah daoisdjasodjiausd holy moly my computer is so slow. help i need to clean it up i thinksty blah daoisdjasodjiausd holy moly my computer is so slow. help i need to clean it up i thinksty blah daoisdjasodjiausd holy moly my computer is so slow. help i need to clean it up i thinksty blah daoisdjasodjiausd holy moly my computer is so slow. help i need to clean it up i thinksty blah daoisdjasodjiausd holy moly my computer is so slow. help i need to clean it up i thinksty blah daoisdjasodjiausd holy moly my computer is so slow. help i need to clean it up i thinksty blah daoisdjasodjiausd holy moly my computer is so slow. help i need to clean it up i think",
+        created_at: "20 sep 2019 5pm",
+        deleted: false
+      },
+      {
+        id: 5,
+        creator: "selin",
+        content: "*sigh*",
+        created_at: "20 sep 2019 6pm",
+        deleted: true
+      },
+      {
+        id: 6,
+        creator: "bot",
+        content: "have some juice my guyyyy, you look thirsty",
+        created_at: "20 sep 2019 5pm",
+        deleted: false
+      }
+    ]
   },
   {
     id: 2,
     type: "single",
     name: "Anchen",
-    avatar: "avatar/two.png"
+    avatar: "avatar/two.png",
+    messages: [
+      {
+        id: 0,
+        creator: "tyler",
+        content: "what whattt",
+        created_at: "20 sep 2019 5pm",
+        deleted: false
+      },
+      {
+        id: 1,
+        creator: "bob",
+        content: "hello there",
+        created_at: "20 sep 2019 2pm",
+        deleted: true
+      },
+      {
+        id: 2,
+        creator: "christopher",
+        content: "wow, i hate you.",
+        created_at: "20 sep 2019 3pm",
+        deleted: false
+      },
+      {
+        id: 3,
+        creator: "anchen",
+        content: "i only drink sweet drinks.",
+        created_at: "20 sep 2019 4pm",
+        deleted: true
+      },
+      {
+        id: 4,
+        creator: "tyler",
+        content:
+          "have some gatorade, you look thirsty blah daoisdjasodjiausd holy moly my computer is so slow. help i need to clean it up i think sty blah daoisdjasodjiausd holy moly my computer is so slow. help i need to clean it up i thinksty blah daoisdjasodjiausd holy moly my computer is so slow. help i need to clean it up i thinksty blah daoisdjasodjiausd holy moly my computer is so slow. help i need to clean it up i thinksty blah daoisdjasodjiausd holy moly my computer is so slow. help i need to clean it up i thinksty blah daoisdjasodjiausd holy moly my computer is so slow. help i need to clean it up i thinksty blah daoisdjasodjiausd holy moly my computer is so slow. help i need to clean it up i thinksty blah daoisdjasodjiausd holy moly my computer is so slow. help i need to clean it up i thinksty blah daoisdjasodjiausd holy moly my computer is so slow. help i need to clean it up i think",
+        created_at: "20 sep 2019 5pm",
+        deleted: false
+      },
+      {
+        id: 5,
+        creator: "selin",
+        content: "*sigh*",
+        created_at: "20 sep 2019 6pm",
+        deleted: true
+      },
+      {
+        id: 6,
+        creator: "bot",
+        content: "have some juice my guyyyy, you look thirsty",
+        created_at: "20 sep 2019 5pm",
+        deleted: false
+      }
+    ]
   },
   {
     id: 3,
     type: "group",
     name: "Anchen/Tyler/Selin",
-    avatar: "avatar/three.png"
+    avatar: "avatar/three.png",
+    messages: [
+      {
+        id: 0,
+        creator: "tyler",
+        content: "what whattt",
+        created_at: "20 sep 2019 5pm",
+        deleted: false
+      },
+      {
+        id: 1,
+        creator: "bob",
+        content: "hello there",
+        created_at: "20 sep 2019 2pm",
+        deleted: true
+      },
+      {
+        id: 2,
+        creator: "christopher",
+        content: "wow, i hate you.",
+        created_at: "20 sep 2019 3pm",
+        deleted: false
+      },
+      {
+        id: 3,
+        creator: "anchen",
+        content: "i only drink sweet drinks.",
+        created_at: "20 sep 2019 4pm",
+        deleted: true
+      },
+      {
+        id: 4,
+        creator: "tyler",
+        content:
+          "have some gatorade, you look thirsty blah daoisdjasodjiausd holy moly my computer is so slow. help i need to clean it up i think sty blah daoisdjasodjiausd holy moly my computer is so slow. help i need to clean it up i thinksty blah daoisdjasodjiausd holy moly my computer is so slow. help i need to clean it up i thinksty blah daoisdjasodjiausd holy moly my computer is so slow. help i need to clean it up i thinksty blah daoisdjasodjiausd holy moly my computer is so slow. help i need to clean it up i thinksty blah daoisdjasodjiausd holy moly my computer is so slow. help i need to clean it up i thinksty blah daoisdjasodjiausd holy moly my computer is so slow. help i need to clean it up i thinksty blah daoisdjasodjiausd holy moly my computer is so slow. help i need to clean it up i thinksty blah daoisdjasodjiausd holy moly my computer is so slow. help i need to clean it up i think",
+        created_at: "20 sep 2019 5pm",
+        deleted: false
+      },
+      {
+        id: 5,
+        creator: "selin",
+        content: "*sigh*",
+        created_at: "20 sep 2019 6pm",
+        deleted: true
+      },
+      {
+        id: 6,
+        creator: "bot",
+        content: "have some juice my guyyyy, you look thirsty",
+        created_at: "20 sep 2019 5pm",
+        deleted: false
+      }
+    ]
   }
 ];
 
