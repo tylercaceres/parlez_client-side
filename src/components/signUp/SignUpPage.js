48c2891563675cac88c2037f03aef3bdbc8ed2fa
Anchen <acmhuang@gmail.com>
2019-09-06 22:23:03 -0400

    implemented axios to signup page

--- src/components/signUp/SignUpPage.js
+++ src/components/signUp/SignUpPage.js
@@ -1,13 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SignUpPage.scss";
 import { makeStyles } from "@material-ui/core/styles";
 import picbox from "../../assets/img/signup-image.jpg";
 import Button from "@material-ui/core/Button";
-
 import TextField from "@material-ui/core/TextField";
 import Grid from "@material-ui/core/Grid";
 import AccountCircle from "@material-ui/icons/AccountCircle";
-import { Checkbox } from "@material-ui/core";
+import axios from "axios";
 
 const useStyles = makeStyles(theme => ({
   margin: {
@@ -20,11 +19,52 @@ const useStyles = makeStyles(theme => ({
 }));
 
 const SignUpPage = () => {
+  // const state = {
+  //   name: name,
+  //   email: email,
+  //   password: password
+  // };
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   const classes = useStyles();
+
+  const onChangeName = e => {
+    setName(e.target.value);
+  };
+
+  const onChangeEmail = e => {
+    setEmail(e.target.value);
+  };
+
+  const onChangePassword = e => {
+    setPassword(e.target.value);
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+    const data = {
+      name: name,
+      email: email,
+      password: password
+    };
+    axios
+      .post("/signup", data)
+      .then(response => {
+        console.log(response);
+        this.props.history.push("/chat");
+      })
+      .catch(error => {
+        console.log("this is the error", error);
+      });
+  };
+
   return (
     <div className="purple-square-container">
       <div className="red-square">
-        <div className="input-box-area">
+        <form onSubmit={onSubmit} className="input-box-area">
           <h1>Sign up</h1>
           <div className={classes.margin}>
             <Grid container spacing={1} alignItems="flex-end">
@@ -32,7 +72,14 @@ const SignUpPage = () => {
                 <AccountCircle />
               </Grid>
               <Grid item>
-                <TextField id="input-with-icon-grid" label="Your Name" />
+                <TextField
+                  id="input-with-icon-grid"
+                  label="Your Name"
+                  name="name"
+                  type="text"
+                  value={name}
+                  onChange={onChangeName}
+                />
               </Grid>
             </Grid>
           </div>
@@ -42,7 +89,14 @@ const SignUpPage = () => {
                 <AccountCircle />
               </Grid>
               <Grid item>
-                <TextField id="input-with-icon-grid" label="Your Email" />
+                <TextField
+                  id="input-with-icon-grid"
+                  label="Your Email"
+                  name="email"
+                  type="text"
+                  value={email}
+                  onChange={onChangeEmail}
+                />
               </Grid>
             </Grid>
           </div>
@@ -52,7 +106,14 @@ const SignUpPage = () => {
                 <AccountCircle />
               </Grid>
               <Grid item>
-                <TextField id="input-with-icon-grid" label="Password" />
+                <TextField
+                  id="input-with-icon-grid"
+                  label="Password"
+                  name="password"
+                  type="text"
+                  value={password}
+                  onChange={onChangePassword}
+                />
               </Grid>
             </Grid>
           </div>
@@ -68,13 +129,14 @@ const SignUpPage = () => {
           </div>
 
           <Button
+            type="submit"
             variant="contained"
             color="primary"
             className={classes.button}
           >
             Submit
           </Button>
-        </div>
+        </form>
         <div className="picture-container">
           <div>
             <img src={picbox} className="picture-box" />
