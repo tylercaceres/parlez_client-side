9467409be4f8e9e6aa273fc62eee10b44f23fddf
tylerccaceres <tyler.caceres@gmail.com>
2019-09-09 16:31:22 -0400

    implmented real time chat feature

--- src/Context.js
+++ src/Context.js
@@ -1,156 +1,126 @@
-import React, { useReducer } from "react";
+import React, {useReducer} from 'react';
 
 const initialMasterState = {
-  username: "rparlot3",
-  userId: 4,
-  friendsView: false,
-  activeChat: null,
-  // hover: null,
-  chatrooms: []
+	userId: null,
+	isLoggedIn: false,
+	friendsView: false,
+	activeChat: null,
+	// hover: null,
+	chatrooms: []
 };
 
 let masterReducer = (state, action) => {
-  switch (action.type) {
-    // case 'LOAD_INITIAL_DATA':
-    //   return {...state, chatrooms: '5'};
-    case "LOAD_INITIAL_DATA":
-      return { ...state, chatrooms: action.data };
-    case "CHAT_VIEW":
-      return { ...state, friendsView: false };
-    case "FRIENDS_VIEW":
-      return { ...state, friendsView: true };
-    case "ACTIVATE_CHAT":
-      return { ...state, friendsView: false, activeChat: action.id };
-    case "HOVER_ON":
-      return { ...state, hover: action.id };
-    case "HOVER_OFF":
-      return { ...state, hover: null };
-    default:
-      throw new Error(`Unsupported action type: ${action.type}`);
-  }
+	switch (action.type) {
+		case 'SIGNUP':
+			return {...state, userId: action.id, isLoggedIn: action.data};
+		case 'LOAD_INITIAL_DATA':
+			return {...state, chatrooms: action.data};
+		case 'CHAT_VIEW':
+			return {...state, friendsView: false};
+		case 'FRIENDS_VIEW':
+			return {...state, friendsView: true};
+		case 'ACTIVATE_CHAT':
+			return {...state, friendsView: false, activeChat: action.id};
+		case 'HOVER_ON':
+			return {...state, hover: action.id};
+		case 'HOVER_OFF':
+			return {...state, hover: null};
+		case 'ADD_MESSAGE':
+			const temp = state.chatrooms.map(chatroom => {
+				if (chatroom.id === action.data.chatroom) {
+					return {...chatroom, messages: [...chatroom.messages, action.data.message]};
+				} else {
+					return {...chatroom};
+				}
+			});
+			return {...state, chatrooms: temp};
+		default:
+			throw new Error(`Unsupported action type: ${action.type}`);
+	}
 };
 
 const ChatViewContext = React.createContext(initialMasterState);
 
 const ChatViewProvider = props => {
-  const [masterState, dispatch] = useReducer(masterReducer, initialMasterState);
+	const [masterState, dispatch] = useReducer(masterReducer, initialMasterState);
 
-  return (
-    <ChatViewContext.Provider value={{ masterState, dispatch }}>
-      {props.children}
-    </ChatViewContext.Provider>
-  );
+	return <ChatViewContext.Provider value={{masterState, dispatch}}>{props.children}</ChatViewContext.Provider>;
 };
 
 /********************************* FRIENDS CONTEXT ***********************************/
 
 const initialFriendState = {
-  selectedFriend: null,
-  friends: [
-    {
-      id: 1,
-      email: "friend_1@email.com",
-      username: "Friend_1",
-      avatar: "/avatar/one.png",
-      status: "busy"
-    },
-    {
-      id: 2,
-      email: "friend_2@email.com",
-      username: "Friend_2",
-      avatar: "/avatar/two.png",
-      status: "busy"
-    },
-    {
-      id: 3,
-      email: "friend_3@email.com",
-      username: "Friend_3",
-      avatar: "/avatar/three.png",
-      status: "busy"
-    }
-  ]
+	selectedFriend: null,
+	friends: []
 };
 
 let friendReducer = (state, action) => {
-  switch (action.type) {
-    case "SELECT_FRIEND":
-      return { ...state, selectedFriend: action.data };
-    default:
-      throw new Error(`Unsupported action type: ${action.type}`);
-  }
+	switch (action.type) {
+		case 'SELECT_FRIEND':
+			return {...state, selectedFriend: action.data};
+		case 'LOAD_FRIENDS':
+			return {...state, friends: action.data};
+		default:
+			throw new Error(`Unsupported action type: ${action.type}`);
+	}
 };
 
 const FriendContext = React.createContext(initialFriendState);
 
 const FriendProvider = props => {
-  const [friendState, dispatch] = useReducer(friendReducer, initialFriendState);
+	const [friendState, dispatchFriend] = useReducer(friendReducer, initialFriendState);
 
-  return (
-    <FriendContext.Provider value={{ friendState, dispatch }}>
-      {props.children}
-    </FriendContext.Provider>
-  );
+	return <FriendContext.Provider value={{friendState, dispatchFriend}}>{props.children}</FriendContext.Provider>;
 };
 
 /********************************* NEW MSG CONTEXT ***********************************/
 
 const initialMsgState = {
-  newMessage: "",
-  charCount: 0,
-  showEmoji: false,
-  msgBtnStatus: true,
-  countDisplay: "0 characters"
+	newMessage: '',
+	charCount: 0,
+	showEmoji: false,
+	msgBtnStatus: true,
+	countDisplay: '0 characters'
 };
 
 let msgReducer = (state, action) => {
-  switch (action.type) {
-    case "NEW_MESSAGE":
-      return { ...state, newMessage: action.data };
-    case "MSG_AND_EMOJI":
-      return { ...state, newMessage: action.data };
-    case "SHOW_EMOJI":
-      return { ...state, showEmoji: !state.showEmoji };
-    case "MESSAGE_SENT":
-      return {
-        ...state,
-        newMessage: "",
-        countDisplay: "0 characters",
-        msgBtnStatus: true
-      };
-    case "CHECK_COUNT":
-      return { ...state, msgBtnStatus: !true };
-    case "UPDATE_COUNT":
-      return {
-        ...state,
-        charCount: action.data
-      };
-    case "DISPLAY_COUNT":
-      return {
-        ...state,
-        countDisplay: `${state.charCount.toString()} characters`
-      };
-    default:
-      throw new Error(`Unsupported action type: ${action.type}`);
-  }
+	switch (action.type) {
+		case 'NEW_MESSAGE':
+			return {...state, newMessage: action.data};
+		case 'MSG_AND_EMOJI':
+			return {...state, newMessage: action.data};
+		case 'SHOW_EMOJI':
+			return {...state, showEmoji: !state.showEmoji};
+		case 'MESSAGE_SENT':
+			return {
+				...state,
+				newMessage: '',
+				countDisplay: '0 characters',
+				msgBtnStatus: true
+			};
+		case 'CHECK_COUNT':
+			return {...state, msgBtnStatus: !true};
+		case 'UPDATE_COUNT':
+			return {
+				...state,
+				charCount: action.data
+			};
+		case 'DISPLAY_COUNT':
+			return {
+				...state,
+				countDisplay: `${state.charCount.toString()} characters`
+			};
+		default:
+			throw new Error(`Unsupported action type: ${action.type}`);
+	}
 };
 
 const MsgContext = React.createContext(initialMsgState);
 
 const MsgProvider = props => {
-  const [msgState, dispatch] = useReducer(msgReducer, initialMsgState);
+	const [msgState, dispatch] = useReducer(msgReducer, initialMsgState);
 
-  return (
-    <MsgContext.Provider value={{ msgState, dispatch }}>
-      {props.children}
-    </MsgContext.Provider>
-  );
+	return <MsgContext.Provider value={{msgState, dispatch}}>{props.children}</MsgContext.Provider>;
 };
 
-export {
-  ChatViewContext,
-  ChatViewProvider,
-  FriendContext,
-  FriendProvider,
-  MsgContext,
-  MsgProvider
-};
+export {ChatViewContext, ChatViewProvider, FriendContext, FriendProvider, MsgContext, MsgProvider};
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,94 +1,94 @@
-import React, { useContext, useEffect, Fragment } from "react";
-import ContactList from "./ContactList/ContactList";
-import FriendList from "./ContactList/FriendList";
-import FriendProfile from "./ContactList/FriendProfile";
-import SearchBar from "./ContactList/SearchBar";
-import ChatHeader from "./ContactList/ChatHeader";
-import MsgChatBox from "./chatArea/MsgChatBox";
-import MsgChatItemList from "./chatArea/MsgChatItemList";
-import "./HomePage.scss";
-import { ChatViewContext, FriendContext } from "../Context";
-import { loadInitialData } from "../server_api";
-import UserAvatar from "./userHeader/UserAvatar";
-import UserButton from "./userHeader/UserAddButton";
-import Divider from "@material-ui/core/Divider";
-import img1 from "../assets/img/dog1.png";
-import img2 from "../assets/img/dog2.png";
-import img3 from "../assets/img/dog3.png";
-import img4 from "../assets/img/dog4.png";
-import img5 from "../assets/img/dog5.png";
+import React, {useContext, useEffect, Fragment} from 'react';
+import ContactList from './ContactList/ContactList';
+import FriendList from './ContactList/FriendList';
+import FriendProfile from './ContactList/FriendProfile';
+import SearchBar from './ContactList/SearchBar';
+import ChatHeader from './ContactList/ChatHeader';
+import MsgChatBox from './chatArea/MsgChatBox';
+import MsgChatItemList from './chatArea/MsgChatItemList';
+import './HomePage.scss';
+import {ChatViewContext, FriendContext} from '../Context';
+import {loadInitialData} from '../server_api';
+import UserAvatar from './userHeader/UserAvatar';
+import UserButton from './userHeader/UserAddButton';
+import Divider from '@material-ui/core/Divider';
+import img1 from '../assets/img/dog1.png';
+import img2 from '../assets/img/dog2.png';
+import img3 from '../assets/img/dog3.png';
+import img4 from '../assets/img/dog4.png';
+import img5 from '../assets/img/dog5.png';
 
 const HomePage = () => {
-  const { masterState, dispatch } = useContext(ChatViewContext);
-  const { friendState } = useContext(FriendContext);
+	const {masterState, dispatch} = useContext(ChatViewContext);
+	const {friendState} = useContext(FriendContext);
 
-  useEffect(() => {
-    loadInitialData(data => {
-      dispatch({
-        type: "LOAD_INITIAL_DATA",
-        data
-      });
-    });
-  });
+	console.log('HOME_PG', masterState);
 
-  return (
-    <body className="layout">
-      <div className="contactsArea">
-        <div className="userHeader">
-          <UserAvatar />
-          <UserButton />
-        </div>
+	console.log('FRIEND_STATE', friendState);
 
-        <Fragment>
-          <ChatHeader />
-        </Fragment>
-        <Divider />
-        <Fragment>
-          <SearchBar />
-        </Fragment>
-        <Fragment>
-          {masterState.friendsView ? <FriendList /> : <ContactList />}
-        </Fragment>
-      </div>
+	// useEffect(() => {
+	//   loadInitialData(data => {
+	//     dispatch({
+	//       type: "LOAD_INITIAL_DATA",
+	//       data
+	//     });
+	//   });
+	// });
 
-      <div>
-        <div className="chatBox">
-          {masterState.activeChat &&
-          masterState.chatrooms.length > 0 &&
-          !masterState.friendsView ? (
-            <>
-              <div className="chatArea">
-                <MsgChatItemList />
-              </div>
-              <div className="chatInput">
-                <MsgChatBox />
-              </div>
-            </>
-          ) : masterState.friendsView && friendState.selectedFriend ? (
-            <FriendProfile />
-          ) : (
-            <div className="emptyChat">
-              <fragment>
-                <img src={img1} alt="dog" />
-              </fragment>
-              <fragment>
-                <img src={img2} alt="dog" />
-              </fragment>
-              <fragment>
-                <img src={img3} alt="dog" />
-              </fragment>
-              <fragment>
-                <img src={img4} alt="dog" />
-              </fragment>
-              <fragment>
-                <img src={img5} alt="dog" />
-              </fragment>
-            </div>
-          )}
-        </div>
-      </div>
-    </body>
-  );
+	return (
+		<body className='layout'>
+			<div className='contactsArea'>
+				<div className='userHeader'>
+					<UserAvatar />
+					<UserButton />
+				</div>
+
+				<Fragment>
+					<ChatHeader />
+				</Fragment>
+				<Divider />
+				<Fragment>
+					<SearchBar />
+				</Fragment>
+				<Fragment>{masterState.friendsView ? <FriendList /> : <ContactList />}</Fragment>
+			</div>
+
+			<div>
+				<div className='chatBox'>
+					{masterState.activeChat && masterState.chatrooms.length > 0 && !masterState.friendsView ? (
+						<>
+							<div className='chatArea'>
+								<MsgChatItemList />
+							</div>
+							<div className='chatInput'>
+								<MsgChatBox />
+							</div>
+						</>
+					) : masterState.friendsView && friendState.selectedFriend ? (
+						<FriendProfile />
+					) : (
+						<div className='emptyChat'>
+							<fragment>
+								<img src={img1} alt='dog' />
+							</fragment>
+							<fragment>
+								<img src={img2} alt='dog' />
+							</fragment>
+							<fragment>
+								<img src={img3} alt='dog' />
+							</fragment>
+							<fragment>
+								<img src={img4} alt='dog' />
+							</fragment>
+							<fragment>
+								<img src={img5} alt='dog' />
+							</fragment>
+						</div>
+					)}
+				</div>
+			</div>
+		</body>
+	);
 };
 
 export default HomePage;
